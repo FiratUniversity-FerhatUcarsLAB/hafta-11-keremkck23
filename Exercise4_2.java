0c254a1485a1c83e3f23f7eb9adb706b51bad09c
Kerem <keremkucuk023@gmail.com>
2025-12-03 21:11:01 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,23 @@
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+    // Main her zaman başlangıç noktasıdır (1)
+    public static void main(String[] args) {                 // 1
+        zippo("rattle", 13);                                 // 2
+    }                                                        // 17
 
-    public static void main(String[] args) {
-        zippo("rattle", 13);
-    }
+    public static void baffle(String blimp) {                // 7
+        System.out.println(blimp);                           // 8
+        zippo("ping", -5);                                   // 9
+    }                                                        // 14
 
-    public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
-    }
-
-    public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+    // Bu metod 2 kez çağrıldığı için giriş satırına iki numara verdik (3 ve 10)
+    public static void zippo(String quince, int flag) {      // 3, 10
+        if (flag < 0) {                                      // 4, 11
+            System.out.println(quince + " zoop");            // 12
+        } else {                                             //
+            System.out.println("ik");                        // 5
+            baffle(quince);                                  // 6
+            System.out.println("boo-wa-ha-ha");              // 15
         }
-    }
+    }                                                        // 13, 16
 }
