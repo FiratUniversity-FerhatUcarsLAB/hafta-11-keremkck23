47ae3137634f8d817b9e3c70b7a8864b49cebe01
Kerem <keremkucuk023@gmail.com>
2025-12-03 21:10:18 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,18 @@
+Ad Soyad : Mehmet Kerem Küçük
+    Öğrenci No : 250541097
+
+
+
 public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+    public static void printAmerican(String day,String month ,int date , int year) {
+        System.out.println(day+", "+month+" "+date+", "+year);
     }
-
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    public static void printEuropean(int date, String month , int year,String day) {
+        System.out.println(date+" "+month+" "+year+", "+day);
     }
-
     public static void main(String[] args) {
-        // TODO: test methods
+        printAmerican("Monday","July",22,2019);
+        printEuropean(22,"July",2019,"Monday");
     }
 }
