57e3bc8b215ac9dacca51afa514441d3d1aa3630
Kerem <keremkucuk023@gmail.com>
2025-12-03 21:13:09 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,6 @@
+Ad Soyad : Mehmet Kerem Küçük
+Öğr No : 250541097
+
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,10 +15,15 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // CEVAP 1: Kod hatasiz calisir (Compiles and runs fine).
+        // Metot gorevini yapar ama donen 42 degeri havada kalir (bosa gider),
+        // cunku bir degiskene atamadik veya ekrana yazdirmadik.
+
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        // CEVAP 2: Bu satir acilirsa DERLEME HATASI verir (Compiler Error).
+        // Cunku 'void' metotlar geriye bir sayi veya deger dondurmez.
+        // Hata: "operator + cannot be applied to void" (Void ile sayi toplanamaz).
     }
 }
